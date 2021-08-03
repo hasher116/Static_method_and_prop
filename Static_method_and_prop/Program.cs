5078f8a454c0779bd54a8380c2018d0bc53a3e80
hasher116 <andrei13081996@mail.ru>
2021-08-03 14:17:47 +0300

    Добавьте файлы проекта.

---
+++ Static_method_and_prop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Static_method_and_prop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stavka.MinSum = 100;
+
+            Stavka stavka1 = new Stavka(120, 10);
+            Console.WriteLine(Stavka.getSum(120, 10, 3));
+
+            Stavka stavka2 = new Stavka(300, 20);
+
+            Console.WriteLine(Stavka.Counter);
+        }
+    }
+}
---
+++ Static_method_and_prop/Stavka.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_method_and_prop
+{
+    class Stavka
+    {
+        public static int Counter { get; private set; }
+        public decimal Sum { get; private set; }
+        public decimal Rate { get; private set; }
+        private static decimal minSum;
+
+        public static decimal MinSum
+        {
+            get { return minSum; }
+            set { if (value > 0) minSum = value; }
+        }
+
+        public Stavka(decimal sum, decimal rate)
+        {
+            if (sum < MinSum) throw new Exception("Ошибка");
+            Sum = sum;
+            Rate = rate;
+            Counter++;
+        }
+
+        public static decimal getSum(decimal sum, decimal rate, int period)
+        {
+            decimal result = sum;
+            for (int i = 1; i <= period; i++)
+            {
+                result = result + (result * rate / 100);
+            }
+            return result;
+        }
+    }
+}
